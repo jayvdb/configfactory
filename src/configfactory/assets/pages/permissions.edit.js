75718ac180ccef80a102f56cd83b7293c69e4b68
Anton Ruhlov <anton@paxful.com>
2018-10-28 11:11:27 +0200

    Updated permission js constants

--- src/configfactory/assets/pages/permissions.edit.js
+++ src/configfactory/assets/pages/permissions.edit.js
@@ -2,10 +2,12 @@ import $ from 'jquery';
 
 $('input[name=obj]').change(function () {
 
+    const ACTION_ASSIGN = 'assign';
+    const ACTION_REMOVE = 'remove';
     const $this = $(this);
     const objectId = $this.val();
     const perm = $this.data('perm');
-    const action = $this.is(':checked') ? 'assign' : 'remove';
+    const action = $this.is(':checked') ? ACTION_ASSIGN : ACTION_REMOVE;
     const path = $this.data('path');
 
     $.post(path, {
@@ -14,7 +16,7 @@ $('input[name=obj]').change(function () {
         perm: perm
     }, function (response) {
         if (response.success) {
-            if (!perm.startsWith('view_') && action === 'assign') {
+            if (!perm.startsWith('view_') && action === ACTION_ASSIGN) {
                 const $view = $this.closest('tr').find('input[data-perm^=view_]');
                 if (!$view.is(':checked')) {
                     $view.prop('checked', true);
